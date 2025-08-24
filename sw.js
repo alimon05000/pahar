8bee73c1b9735f9c38ef06788f9be94405b8aa4b
alimon05000 <alimsrifrv23@gmail.com>
2025-08-24 22:34:08 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+// sw.js
+const CACHE_NAME = 'workout-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
